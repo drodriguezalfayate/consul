fff7e717c3efab0ec64459bf4ee56858f38a2086
Diego <disarrc@gmail.com>
2017-06-12 22:45:57 +0200

    Corregidos textos correos.

--- app/helpers/budget_headings_helper.rb
+++ app/helpers/budget_headings_helper.rb
@@ -2,7 +2,7 @@ module BudgetHeadingsHelper
 
   def budget_heading_select_options(budget)
     budget.headings.order_by_id.map do |heading|
-      [heading.name, heading.id]
+      [heading.long_name, heading.id]
     end
   end
 
