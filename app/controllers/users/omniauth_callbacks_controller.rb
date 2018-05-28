9b84931d731b04a2581d773216db2bc520c027de
Unknown <sergio@an2.es>
2018-05-28 13:08:01 +0200

    Revert "Modificado comportamiento de la verificación de residencia"
    This reverts commit 2951ba92d253da9f62f48a14c983998891dab95a.

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -26,62 +26,62 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   private
 
-  def sign_in_with( feature, provider )
+    def sign_in_with( feature, provider )
+      
+      raise ActionController::RoutingError.new('Not Found') unless Setting["feature.#{feature}"]
 
-    raise ActionController::RoutingError.new('Not Found') unless Setting["feature.#{feature}"]
+      auth = env["omniauth.auth"]
+      
+      if auth.info.invalid_credentials
+	
+	if provider.to_s == 'ldap'
+          redirect_to new_ldap_path, alert: 'Datos de acceso incorrectos.'
+        else
+          redirect_to new_codigo_path, alert: 'Datos de acceso incorrectos.'
+        end
 
-    auth = env["omniauth.auth"]
+        return
 
-    if auth.info.invalid_credentials
-
-      if provider.to_s == 'ldap'
-        redirect_to new_ldap_path, alert: 'Datos de acceso incorrectos.'
-      else
-        redirect_to new_codigo_path, alert: 'Datos de acceso incorrectos.'
       end
 
-      return
-
-    end
-
-    if provider.to_s == 'ldap'
-      auth.info.verified = true
-    end
-
-    identity = Identity.first_or_create_from_oauth(auth)
-    @user = current_user || identity.user || User.first_or_initialize_for_oauth(auth)
-
-
-    if provider.to_s == 'codigo'
-      auth.info.verified = true
-
-      @user.skip_email_validation = true
-      @user.document_type   = auth.info.document_type
-      @user.document_number = auth.info.document_number
-      @user.confirmed_at = Time.current
-      @user.verified_at  = Time.current
-    end
-
-    if save_user(@user)
-
-      identity.update(user: @user)
-      sign_in_and_redirect @user, event: :authentication
-      set_flash_message(:notice, :success, kind: "#{provider}".capitalize) if is_navigational_format?
-
-    else
-
-      session["devise.#{provider}_data"] = auth
-
       if provider.to_s == 'ldap'
-        redirect new_ldap_path
+        auth.info.verified = true
+      end
+
+      identity = Identity.first_or_create_from_oauth(auth)
+      @user = current_user || identity.user || User.first_or_initialize_for_oauth(auth)
+      
+      
+      if provider.to_s == 'codigo'
+        auth.info.verified = true
+
+        @user.skip_email_validation = true
+        @user.document_type   = auth.info.document_type
+        @user.document_number = auth.info.document_number
+        @user.confirmed_at = Time.current
+        @user.verified_at  = Time.current
+      end
+      
+      if save_user(@user)
+
+        identity.update(user: @user)
+        sign_in_and_redirect @user, event: :authentication
+        set_flash_message(:notice, :success, kind: "#{provider}".capitalize) if is_navigational_format?
+
       else
-        redirect_to new_user_registration_url
+
+        session["devise.#{provider}_data"] = auth
+
+        if provider.to_s == 'ldap'
+	  redirect new_ldap_path
+        else
+          redirect_to new_user_registration_url
+        end
       end
     end
-  end
 
-  def save_user(user)
-    @user.save || @user.save_requiring_finish_signup
-  end
+    def save_user(user)
+      @user.save || @user.save_requiring_finish_signup
+    end
 
 end
