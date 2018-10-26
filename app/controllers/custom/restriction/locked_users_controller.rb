6ba0ca401cbed9667430b45637d886596308065d
Unknown <sergio@an2.es>
2018-10-26 14:37:51 +0200

    Optimizado controlador de locked users para mostrar correctamente los errores

--- app/controllers/custom/restriction/locked_users_controller.rb
+++ app/controllers/custom/restriction/locked_users_controller.rb
@@ -20,27 +20,25 @@ class Restriction::LockedUsersController < Restriction::BaseController
   def preview
     params[:budget_locked_user][:document_number].upcase
     @locked_user = Budget::LockedUser.new(locked_user_params)
-    @already_locked = false
-    if @locked_user.valid?
-      old_locked_user = Budget::LockedUser.find_by(
-        budget_id: locked_user_params[:budget_id],
-        document_type: locked_user_params[:document_type],
-        document_number: locked_user_params[:document_number])
+    old_locked_user = Budget::LockedUser.find_by(
+      budget_id: locked_user_params[:budget_id],
+      document_type: locked_user_params[:document_type],
+      document_number: locked_user_params[:document_number])
+    @already_locked = old_locked_user.present?
 
-      if old_locked_user.present?
-        @already_locked = true
-        @locked_time = old_locked_user.created_at
-      else
-        document_verification = Verification::Management::Document.new(document_verification_params)
-        if document_verification.valid?
-          @in_census = false
-          if document_verification.in_census?
-            @in_census = true
-          end
-          @has_voted = false
-          if document_verification.user?
-            @has_voted = Budget::Ballot::Line.joins(:ballot).where(budget_ballots: { user_id: User.find_by(document_number: document_verification_params[:document_number])}).exists?
-          end
+    if @already_locked
+      @in_census = true
+      @locked_time = old_locked_user.created_at
+    elsif @locked_user.valid?
+      document_verification = Verification::Management::Document.new(document_verification_params)
+      if document_verification.valid?
+        @in_census = false
+        if document_verification.in_census?
+          @in_census = true
+        end
+        @has_voted = false
+        if document_verification.user?
+          @has_voted = Budget::Ballot::Line.joins(:ballot).where(budget_ballots: { user_id: User.find_by(document_number: document_verification_params[:document_number])}).exists?
         end
       end
     else
